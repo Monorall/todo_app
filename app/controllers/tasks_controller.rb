f2c20469adaa971838cdc03323471dff868a3e6b
monoral <goldflesher@gmail.com>
2023-06-29 22:40:23 +0300

    Add edit

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -31,7 +31,7 @@ class TasksController < ApplicationController
 
   def update
     if @task.update(task_params)
-      redirect_to @task, notice: 'Task was successfully updated.'
+      redirect_to tasks_path, notice: 'Task was successfully updated.'
     else
       render :edit
     end
